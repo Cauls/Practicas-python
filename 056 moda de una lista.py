1061de600fff04fa67e2c6f9d3b25ec0588880e3
Sol <alvaro.figueroa3206@gmail.com>
2025-10-01 23:33:40 +0200

    Moda

--- 056 moda de una lista.py
+++ 056 moda de una lista.py
@@ -1,9 +1,14 @@
-lista = [1, 1, 3, 1, 5, 6, 6, 1, 2, 3, 4]
+lista = [1, 1, 3, 1, 5, 6, 6, 1, 2, 3, 4, 1]
 moda = [0, 0]
-contador = 0
+previo = 0
+candidato = [0, 0]
 lista.sort()
 for i in lista:
-    if i == moda[0]:
-        contador += 1
-
+    actual = i
+    if candidato[0] == i:
+        candidato[1] += 1
+    else:
+        candidato = [i, 1]
+    if candidato[1] > moda[1]:
+        moda = candidato
 print(moda)

d3a0cdf958025d04e604153b6a6e49643bbdec1d
Sol <alvaro.figueroa3206@gmail.com>
2026-01-09 11:05:03 +0100

    Impostor terminado 100% (creo?)

--- Impostor/funciones.py
+++ Impostor/funciones.py
@@ -1,5 +1,17 @@
 import random
 
+#Funcion para asignar jugadores, pide la cantidad de jugadores, e itera en un for x veces siendo x la cantidad de jugadores, cada iteración pide un jugador que será añadido sin rol alguno al diccionario
+def asignarJugadores(jugadores):
+    cantidad = 0
+    while cantidad < 3:
+        cantidad = int(input("Inserta el número de jugadores que van a jugar: "))
+        if cantidad < 3: 
+            print("El número mínimo de jugadores es 3")
+    for i in range(cantidad):
+        entrada = input("Inserta el nombre del jugador a añadir: ")
+        jugadores[entrada] = "Nan"
+
+#Según la cantidad de jugadores, se asigna un número aleatorio, el jugador en ese indice del diccionario será asignador como impostor, el resto como civiles
 def impostor(jugadores):
     impostor = random.randint(0, len(jugadores)-1)
     index = 0
@@ -9,7 +21,8 @@ def impostor(jugadores):
         else:
             jugadores[i] = "Civil"
         index+=1
-    
+
+#Similar a la anterior funcion, en funcion de la cantidad de palabras en el diccionario, escogerá una al azar por indice y devolverá un array con la palabra y la pista
 def selPalabra(palabras):
     palabra = ["", ""]
     elegida = random.randint(0, len(palabras)-1)
@@ -20,6 +33,7 @@ def selPalabra(palabras):
         index+=1
     return palabra
 
+#El bucle de inicio, recorre todo el diccionario de jugadores e imprime la palabra o la pista a cada jugador dependiendo de que haya salido como impostor o como civil. También tengo un arte de ASCII ahi como introducción pero se rompe un poco la verdad
 def inicio(jugadores, palabras):
     print("=======================================================\n  _____ __  __ _____   ____   _____ _______ ____  _____ \n |_   _|  \/  |  __ \ / __ \ / ____|__   __/ __ \|  __ \ \n   | | | \  / | |__) | |  | | (___    | | | |  | | |__) | \n   | | | |\/| |  ___/| |  | |\___ \   | | | |  | |  _  / \n  _| |_| |  | | |    | |__| |____) |  | | | |__| | | \ \  \n |_____|_|  |_|_|     \____/|_____/   |_|  \____/|_|  \_\ \n=======================================================")
     impostor(jugadores)
@@ -33,6 +47,7 @@ def inicio(jugadores, palabras):
             input("")
         print("\n"*50)
 
+#Funcion que recibiendo la lista de jugadores y el jugador que se ha elegido para eliminar, decide si saltar la ronda o eliminar a un jugador del diccionario. Una vez lo elimina también notifica de que rol cumplía el jugador
 def eliminar(jugadores, eliminado):
     if eliminado == "Paso":
         print("Se ha saltado la ronda")
@@ -47,13 +62,18 @@ def eliminar(jugadores, eliminado):
             jugadores.pop(eliminado)
 
 
-
+#El bucle completo une al resto, este lo explicaré mas parte por parte
 def turno(jugadores):
-    eliminado = ""
+    restantes = ""      #<--Variable para almacenar mas adelante los jugadores que quedan
     for i in jugadores:
-        input(f"Jugador {i}, inserte su frase: \n")
-    while eliminado not in jugadores.keys() and eliminado != "Paso":
-        print(f"Jugadores a eliminar: {jugadores.keys()}")
-        eliminado = input("Inserte el nombre del jugador votado o 'Paso' para saltar la ronda:\n")
-    eliminar(jugadores, eliminado)
+        restantes += f"{i} "    #<--Este bucle simplemente añade a restantes los jugadores que quedan
+    eliminado = ""    #<--Variable vacía donde se ubicará el nombre del jugador votado
+    for i in jugadores:
+        input(f"Jugador {i}, inserte su frase: \n")     #<--Aquí se pide la frase de cada jugador
+    while eliminado not in jugadores.keys() and eliminado != "Paso":    #<--Control de entrada, sigue preguntado constantemente a quien se vota mientras el jugador no exista en el diccionario ni se escriba 'Paso'
+        print(f"Jugadores a eliminar: {restantes}")     #<--Imprime los jugadores restantes para no perderse a lo largo de la partida
+        eliminado = input("Inserte el nombre del jugador votado o 'Paso' para saltar la ronda:\n")    #<--El input del voto
+        if eliminado not in jugadores.keys():
+            print(f"El jugador {eliminado} no se ha encontrado")    #<--De nuevo control de entrada, si el jugador no existe, avisará antes de repetir el bucle
+    eliminar(jugadores, eliminado)    #<--Una vez se vota adecuadamente al jugador, este es mandado a eliminar
     
--- Impostor/impostor.py
+++ Impostor/impostor.py
@@ -1,10 +1,6 @@
-from funciones import inicio, turno
-jugadores = {
-    "Figue":"Nan",
-    "Misael":"Nan",
-    "Sadesu":"Nan",
-    "Wassim":"Nan"
-}
+#Import de las funciones definidas en el archivo funciones.py, a su vez, abajo tenemos un diccionario vacio de jugadores junto a un diccionario de 20 palabras junto a sus pistas
+from funciones import inicio, turno, asignarJugadores 
+jugadores = {}
 palabras = { 
     "Playa":"Bañador",
     "Coche":"Carretera",
@@ -28,9 +24,17 @@ palabras = {
     "Programador":"Copilot"
     }
 
+#Al comenzar la partida se usa asignarJugadores, esto pedirá la cantidad de jugadores junto al nombre de cada uno
+asignarJugadores(jugadores)
+
+#El comienzo de la partida, inicio asignará roles, le dará a cada uno su palabra e iniciará el bucle jugable
 inicio(jugadores, palabras)
+
+#Bucle muy sencillo, mientras haya al menos un impostor y mas de dos jugadores, la partida continuará, esto se debe a que los civiles perderán si hay tantos civiles como impostores y el impostor perderá si lo eliminan
 while "Impostor" in jugadores.values() and len(jugadores) > 2:
     turno(jugadores)
+
+#Sencillo condicional, si hay un impostor cuando la partida acaba, han ganado los impostores, si no, los civiles
 if "Impostor" in jugadores.values():
     print("Victoria de los impostores")
 else:

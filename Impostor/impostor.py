4a8078579bf94c783e535faf8570213d084d88c4
Sol <alvaro.figueroa3206@gmail.com>
2026-01-11 21:15:16 +0100

    No veas

--- Impostor/impostor.py
+++ Impostor/impostor.py
@@ -1,5 +1,6 @@
 #Import de las funciones definidas en el archivo funciones.py, a su vez, abajo tenemos un diccionario vacio de jugadores, un diccionario vacio donde ir almacenando la respuesta de cada jugador y un diccionario de 20 palabras junto a sus pistas
-from funciones import inicio, turno, asignarJugadores 
+from funciones import inicio, turno, asignarJugadores
+repetir = True
 numRondas = 3
 index = 0
 jugadores = {}
@@ -27,19 +28,31 @@ palabras = {
     "Programador":"Copilot"
 }
 
-#Al comenzar la partida se usa asignarJugadores, esto pedirá la cantidad de jugadores junto al nombre de cada uno
-asignarJugadores(jugadores, frases)
+while(repetir):
+    #Al comenzar la partida se usa asignarJugadores, esto pedirá la cantidad de jugadores junto al nombre de cada uno
+    asignarJugadores(jugadores, frases)
 
-#El comienzo de la partida, inicio asignará roles, le dará a cada uno su palabra e iniciará el bucle jugable
-inicio(jugadores, palabras)
+    #El comienzo de la partida, inicio asignará roles, le dará a cada uno su palabra e iniciará el bucle jugable
+    inicio(jugadores, palabras)
 
-#Bucle muy sencillo, mientras haya al menos un impostor, no se superen el numero de rondas y  haya mas de dos jugadores, la partida continuará, esto se debe a que los civiles perderán si hay tantos civiles como impostores y el impostor perderá si lo eliminan
-while "Impostor" in jugadores.values() and len(jugadores) > 2 and index < numRondas:
-    turno(jugadores, frases)
-    index+=1
+    #Bucle muy sencillo, mientras haya al menos un impostor, no se superen el numero de rondas y  haya mas de dos jugadores, la partida continuará, esto se debe a que los civiles perderán si hay tantos civiles como impostores y el impostor perderá si lo eliminan
+    while "Impostor" in jugadores.values() and len(jugadores) > 2 and index < numRondas:
+        turno(jugadores, frases)
+        index+=1
 
-#Sencillo condicional, si hay un impostor cuando la partida acaba, han ganado los impostores, si no, los civiles
-if "Impostor" in jugadores.values():
-    print("Victoria de los impostores")
-else:
-    print("Victoria de los civiles")
+    #Sencillo condicional, si hay un impostor cuando la partida acaba, han ganado los impostores, si no, los civiles
+    if "Impostor" in jugadores.values():
+        print("Victoria de los impostores")
+    else:
+        print("Victoria de los civiles")
+    
+    #Este ultimo switch es el que decidirá si se repite o no la partida, en caso de una S, la partida se repetirá, en caso de una N o cualquier otro valor, la partida no se repetirá
+    eleccion = input("Quieres repetir la partida? S/N")
+    match eleccion:
+        case "S":
+            repetir = True
+        case "N":
+            repetir = False
+        case "Default":
+            repetir = False
+            print("No se repetirá la partida por entrada errónea")
